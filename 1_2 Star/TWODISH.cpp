db8e717e2782cb20845c024ef6e26426c9f80b2c
AMAN KESHRI <58985172+Aman-Keshri@users.noreply.github.com>
2022-06-02 10:17:12 +0530

    Add files via upload

---
+++ 1_2 Star/TWODISH.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,a,b,c;
+	    cin>>n>>a>>b>>c;
+	    
+	    if((a+c >= n) && (b >= n))
+	    cout<<"YES"<<"\n";
+	    else
+	    cout<<"NO"<<"\n";
+	}
+	return 0;
+}
